73b1c46a15fd48af2b4ceacab7f7ed65216446fd
Administrator <Administrator@192.168.1.40>
2017-08-31 17:28:27 +0900

    Update calendar main

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -3,8 +3,13 @@ package honux.calendar;
 public class Calendar {
 
 	public static void main(String[] args) {
-		System.out.println("hellow, Calendar");
-
+		System.out.println(" 일    월    화    수    목    금    토");
+		System.out.println("30 31 01 02 03 04 05");
+		System.out.println("06 07 08 09 10 11 12");
+		System.out.println("13 14 15 16 17 18 19");
+		System.out.println("20 21 22 23 24 25 26");
+		System.out.println("27 28 29 30 31");
+		
 	}
 
 }
